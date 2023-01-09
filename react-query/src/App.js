823354df479f6153d1a7d07e1751c2a095c81f63
Nikita <lavro.nikita123@gmail.com>
2023-01-09 16:55:41 -0500

    Basic App Routing

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,10 +1,35 @@
+//CSS
 import "./App.css";
+
+//Components
+import HomePage from "./components/HomePage";
+import RQSuperHeroesPage from "./components/RQSuperHeroesPage";
+import SuperHeroesPage from "./components/SuperHeroesPage";
+
+//Router
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroesPage />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
+          <Route path="/s" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
