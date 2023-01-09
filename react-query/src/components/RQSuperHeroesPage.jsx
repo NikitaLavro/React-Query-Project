5ffb4762cc1833a3c79d34e207172cb5fadf57b9
Nikita <lavro.nikita123@gmail.com>
2023-01-09 17:55:31 -0500

    basic data fetching with useQuery hook

--- react-query/src/components/RQSuperHeroesPage.jsx
+++ react-query/src/components/RQSuperHeroesPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+//React Query
+import { useQuery } from "react-query";
+
+//Axios
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroesPage</div>;
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
+  );
 };
 
 export default RQSuperHeroesPage;
