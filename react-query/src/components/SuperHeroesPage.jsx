b6171d9007a9c8c3499296bf1dc1d5e056b3f524
Nikita <lavro.nikita123@gmail.com>
2023-01-09 18:01:11 -0500

    basic error handling

--- react-query/src/components/SuperHeroesPage.jsx
+++ react-query/src/components/SuperHeroesPage.jsx
@@ -4,17 +4,27 @@ import axios from "axios";
 const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    setIsLoading(true);
-    axios.get("http://localhost:4000/superheroes").then((res) => {
-      setData(res.data);
+    try {
+      setIsLoading(true);
+      axios.get("http://localhost:4000/suprheroes").then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setError(error.message);
       setIsLoading(false);
-    });
+    }
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>{error}</h2>;
   }
 
   return (
