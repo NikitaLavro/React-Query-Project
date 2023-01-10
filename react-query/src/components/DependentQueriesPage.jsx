087cac60d9b8530dc039d9861a0751e32a506469
Nikita <lavro.nikita123@gmail.com>
2023-01-10 17:26:43 -0500

    fetchCoursesByChannelId implementation

--- react-query/src/components/DependentQueriesPage.jsx
+++ react-query/src/components/DependentQueriesPage.jsx
@@ -7,9 +7,17 @@ const DependentQueriesPage = ({ email }) => {
     return axios.get(`http://localhost:4000/users/${email}`);
   };
 
+  const fetchCoursesByChannelId = (channelId) => {
+    return axios.get(`http://localhost:4000/users/${channelId}`);
+  };
+
   const { data: user } = useQuery(["user, email"], () =>
     fetchUserByEmail(email)
   );
+
+  useQuery(["courses", channelId], () => fetchCoursesByChannelId(channelId), {
+    enabled: !!channelId,
+  });
 
   const channelId = user?.data.channelId;
 
