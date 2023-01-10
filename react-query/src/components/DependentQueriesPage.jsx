584e8f97a1d47290a3f376d90fc0a8a1c3720636
Nikita <lavro.nikita123@gmail.com>
2023-01-10 17:14:47 -0500

    fetchUserByEmail function

--- react-query/src/components/DependentQueriesPage.jsx
+++ react-query/src/components/DependentQueriesPage.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const DependentQueriesPage = ({ email }) => {
+  const fetchUserByEmail = (email) => {
+    return axios.get(`http://localhost:4000/users/${email}`);
+  };
+
+  const { data: user } = useQuery(["user, email"], () =>
+    fetchUserByEmail(email)
+  );
+
+  const channelId = user?.data.channelId;
+
   return <div>DependentQueriesPage</div>;
 };
 
