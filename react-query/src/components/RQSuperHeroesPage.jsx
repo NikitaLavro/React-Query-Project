b2bfb2eb41230c57d6586d3a64a898a392d377eb
Nikita <lavro.nikita123@gmail.com>
2023-01-09 19:13:34 -0500

    data transformation with select

--- react-query/src/components/RQSuperHeroesPage.jsx
+++ react-query/src/components/RQSuperHeroesPage.jsx
@@ -7,14 +7,22 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("https://jsonplaceholder.typicode.com/posts");
+  return axios.get("http://localhost:4000/superheroes");
 };
 
 const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
-    { enabled: false, onSuccess: onFetchSuccess, onError: onFetchError }
+    {
+      enabled: false,
+      onSuccess: onFetchSuccess,
+      onError: onFetchError,
+      select: (data) => {
+        const superHeroNames = data.data.map((hero) => hero.name);
+        return superHeroNames;
+      },
+    }
   );
 
   function onFetchSuccess(data) {
@@ -37,9 +45,12 @@ const RQSuperHeroesPage = () => {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch Heroes</button>
-      {data?.data.map((hero) => (
+      {/*data?.data.map((hero) => (
         <div key={hero.id}>{hero.title}</div>
-      ))}
+      ))*/}
+      {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>;
+      })}
     </>
   );
 };
